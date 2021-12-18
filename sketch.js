7c18ce50d98f30fdb47d289e8f39bc6ea3c1b587
superduper11 <95068411+superduper11@users.noreply.github.com>
2021-12-18 16:24:00 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var clicks = 0;
+var cursor,cursorImg;
+
+function preload()
+{
+  cursorImg = loadImage("cursor.png")
+}
+
+function setup(){
+  createCanvas(window.innerWidth,window.innerHeight);
+  cursor = createSprite(window.innerWidth/2,window.innerHeight/2,30,30);
+  cursor.addImage("cursor",cursorImg);
+  cursor.scale=0.2;
+  
+}
+
+function draw() {
+  background("white")
+  cursor.x=World.mouseX
+  cursor.y=World.mouseY
+  drawSprites();
+  fill("black")
+  textSize(30)
+  text("Clicks: "+clicks,window.innerWidth/2-55,window.innerHeight/2)
+}
+
+function mouseClicked()
+{
+  clicks=clicks+1;
+}
+function touchend()
+{
+  clicks=clicks+1;
+}
